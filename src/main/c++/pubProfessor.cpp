291f8591da2f1a87173d1f489c2a370ee2ed4f96
ChelseaXu <Chelsea.Xu.7@gmail.com>
2015-10-19 09:58:52 -0400

    Initialized pubProfessor.cpp

---
+++ src/main/c++/pubProfessor.cpp
@@ -0,0 +1,195 @@
+#include "pubProfessor.h" 
+
+
+int pubProfessor::getAbstracts(){
+    
+}
+
+int pubProfessor::getJournalArticles(){
+    
+}
+int pubProfessor::getBooks(){
+    
+}
+int pubProfessor::getBooksEdited(){
+    
+}
+int pubProfessor::getBookChapters(){
+    
+}
+int pubProfessor::getEditorLetters(){
+    
+}
+int pubProfessor::getCaseReports(){
+    
+}
+int pubProfessor::getClinicalGuidelines(){
+    
+}
+int pubProfessor::getCommentaries(){
+    
+}
+int pubProfessor::getConferenceProceedings(){
+    
+}
+int pubProfessor::getEditorials(){
+    
+}
+int pubProfessor::getInvitedArticles(){
+    
+}
+int pubProfessor::getMagazineEntries(){
+    
+}
+int pubProfessor::getManuals(){
+    
+}
+int pubProfessor::getMonographs(){
+    
+}
+int pubProfessor::getMultimedia(){
+    
+}
+int	pubProfessor::getNewsletterArticles(){
+    
+}
+int pubProfessor::getNewspaperArticles(){
+    
+}
+int pubProfessor::getStudentPubs(){
+    
+}
+int pubProfessor::getWebsitesVideos(){
+    
+}
+int pubProfessor::getWorkingPapers();{
+    
+}
+
+void pubProfessor::incAbstracts(){
+    
+}
+void pubProfessor::incJournalArticles(){
+    
+}
+void pubProfessor::incBooks(){
+    
+}
+void pubProfessor::incBooksEdited(){
+    
+}
+void pubProfessor::incBookChapters(){
+    
+}
+void pubProfessor::incEditorLetters(){
+    
+}
+void pubProfessor::incCaseReports(){
+    
+}
+void pubProfessor::incClinicalGuidelines(){
+    
+}
+void pubProfessor::incCommentaries(){
+    
+}
+void pubProfessor::incConferenceProceedings(){
+    
+}
+void pubProfessor::incEditorials(){
+    
+}
+void pubProfessor::incInvitedArticles(){
+    
+}
+void pubProfessor::incMagazineEntries(){
+    
+}
+void pubProfessor::incManuals(){
+    
+}
+void pubProfessor::incMonographs(){
+    
+}
+void pubProfessor::incMultimedia(){
+    
+}
+void pubProfessor::incNewsletterArticles(){
+    
+}
+void pubProfessor::incNewspaperArticles(){
+    
+}
+void pubProfessor::incStudentPubs(){
+    
+}
+void pubProfessor::incWebsitesVideos(){
+    
+}
+void pubProfessor::incWorkingPapers(){
+    
+}
+
+void pubProfessor::decAbstracts(){
+    
+}
+void pubProfessor::decJournalArticles(){
+    
+}
+void pubProfessor::decBooks(){
+    
+}
+void pubProfessor::decBooksEdited(){
+    
+}
+void pubProfessor::decBookChapters(){
+    
+}
+void pubProfessor::decEditorLetters(){
+    
+}
+void pubProfessor::decCaseReports(){
+    
+}
+void pubProfessor::decClinicalGuidelines(){
+    
+}
+void pubProfessor::decCommentaries(){
+    
+}
+void pubProfessor::decConferenceProceedings(){
+    
+}
+void pubProfessor::decEditorials(){
+    
+}
+void pubProfessor::decInvitedArticles(){
+    
+}
+void pubProfessor::decMagazineEntries(){
+    
+}
+void pubProfessor::decManuals();{
+    
+}
+void pubProfessor::decMonographs(){
+    
+}
+void pubProfessor::decMultimedia(){
+    
+}
+void pubProfessor::decNewsletterArticles(){
+    
+}
+void pubProfessor::decNewspaperArticles(){
+    
+}
+void pubProfessor::decStudentPubs(){
+    
+}
+void pubProfessor::decWebsitesVideos(){
+    
+}
+void pubProfessor::decWorkingPapers(){
+    
+}
