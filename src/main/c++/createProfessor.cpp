9bbf4619b4652fb4eeb06d97154b0f427c55e5ba
Curtis Vermeeren <curtis.vermeeren@gmail.com>
2015-10-23 09:16:53 -0400

    Implemented createProfessor

---
+++ src/main/c++/createProfessor.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <vector>
+#include "pubProfessor.h"
+//#include "granProfessor.h"
+//#include "teachProfessor.h"
+//#include "presProfessor.h"
+#include "createProfessor.h"
+
+using namespace std;
+
+template <class T>
+createProfessor<T>::createProfessor(){}
+
+template <class T>
+T* createProfessor<T>::createNewProfessor(string professorName, vector<vector<string>>& csv, int begRow){
+    //If type is of pubProfessor do
+    return createNewPubProfessor(professorName,csv,begRow);
+    //If type is of teachProfessor do
+
+    //If type is of granProfessor do
+
+    //If type is of presProfessor do
+
+}
+
+template <class T>
+pubProfessor* createProfessor<T>::createNewPubProfessor(string professorName, vector<vector<string>>& csv,int begRow){
+    //What is domain in prubprofessor constructor?
+    string domain = "Domain";
+    pubProfessor newProf = pubProfessor(professorName,domain);
+    int entries = csv.size();//Total number of entries in csv
+    int currLine = 0; //current line being checked
+    while (currLine < entries){ //Search every entry of csv
+        //If a line begins with the beginning number of the member create an entry for that publication
+        if (csv[currLine][0] = begRow){
+            //Get all the valuces for the entry method from csv
+            string pubStatus, pubType, pubRole, pubName, pubAuthors, pubTitle;
+            pubStatus = csv[currLine][6];
+            pubType = csv[currLine][8];
+            pubRole = csv[currLine][11];
+            pubName = csv[currLine][15];
+            pubAuthors = csv[currLine][37];
+            pubTitle = csv[currLine][38];
+            //Add the new entry to the prof
+            newProf.addEntry(pubStatus,pubType,pubRole,pubName,pubAuthors,pubTitle);
+        }
+        //Move to the next entry
+        currLine++;
+    }
+    //Return the new professor after all entries have been added
+    return newProf;
+
+}
--- src/main/c++/createProfessor.h
+++ src/main/c++/createProfessor.h
@@ -2,9 +2,11 @@
 #include <string>
 #include <vector>
 #include "pubProfessor.h"
+/** Not currently implemented
 #include "granProfessor.h"
 #include "teacProfessor.h"
 #include "presProfessor.h"
+*/
 
 using namespace std;
 
@@ -12,12 +14,16 @@ template<class T>
 class createProfessor {
 public:
 	//Create professor object, filling in all information and pass reference back
-	//This method should retrieve all relivant rows from csv 2D array and pass to appropriate private construct function
+	//This function should retrieve all relivant rows from csv 2D array and pass to appropriate private construct function
+	createProfessor();
 	static T* createNewProfessor(string professorName, vector<vector<string>>& csv, int begRow);
 private:
 	//Should create professor objects of particular professor type and pass back
-	static pubProfessor* createNewPubProfessor(string professorName, vector<vector<string>>& csv);
+	static pubProfessor* createNewPubProfessor(string professorName, vector<vector<string>>& csv, int begRow);
+
+	/** Not currently implemented//
 	static granProfessor* createNewGrantProfessor(string professorName, vector<vector<string>>& csv);
 	static teacProfessor* createNewTeachingProfessor(string professorName, vector<vector<string>>& csv);
 	static presProfessor* createNewPresProfessor(string professorName, vector<vector<string>>& csv);
+	*/
 };
