3cc5253f315c34054fd480d81ad3b8bf6fa2ad75
James Allen <jallen97@uwo.ca>
2015-11-09 22:43:55 -0500

    added legendddddfdsfnsdfkdsn

--- src/main/c++/piechartwidget.cpp
+++ src/main/c++/piechartwidget.cpp
@@ -12,36 +12,38 @@ PieChartWidget::PieChartWidget(QWidget *parent): QWidget(parent)
 
 void PieChartWidget::paintEvent(QPaintEvent *)
 {
-
-    int i, current, total = 0;
+    int i, j, current, total = 0;
     QPainter painter(this);
-    QRectF size = QRectF(10, 10, this->width()-10, this->width()-10);
+    QRectF size = QRectF(10, 10, this->height()-10, this->height()-10);
     int currentHue = 0, last_slice = 0, current_slice;
-    std::vector< std::pair< std::string, QColor>> legendVec;
 
     //get total number of pubs
     for(i = 0; i < data.size(); i++){
-        total += static_cast<int>(data.at(i).second.size());
+        for(j = 0 ; j < data.at(i).second.size(); j++){
+            total += data.at(i).second.at(j).second;
+        }
     }
 
     cout << total << endl;
 
     //gen pie slices
     for(i=0; i < data.size(); i++){
-        QColor current_color;
-        current_color.setHsv(currentHue,255,127);
-        painter.setBrush(current_color);
-        current = static_cast<int>(data.at(i).second.size());
-        cout << current << endl;
-        current_slice = ((double)current/total)*360;
-        painter.drawPie(size, last_slice*16, current_slice*16);
-        last_slice += current_slice;
-        currentHue += 17;
-        std::pair< std::string, QColor> cur_legend;
-        cur_legend.first = data.at(i).first;
-        cur_legend.second = current_color;
-        legendVec.push_back(cur_legend);
+        current = 0;
+        for(j = 0 ; j < data.at(i).second.size(); j++){
+            current += data.at(i).second.at(j).second;
+        }
+        if(current > 0){
+            QColor current_color;
+            current_color.setHsv(currentHue,210,255);
+            painter.setBrush(current_color);
+            cout << current << endl;
+            current_slice = ((double)current/total)*360;
+            painter.drawPie(size, last_slice*16, current_slice*16);
+            last_slice += current_slice;
+            currentHue += 17;
+        }
     }
+
 
 
 }
@@ -51,5 +53,6 @@ void PieChartWidget::setData(std::vector<std::pair<std::string,std::vector<std::
     data = vector;
     repaint();
     update();
+
 }
 
