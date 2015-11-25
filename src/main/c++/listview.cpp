5c3a397f27b32520145df9941b76478d3a6543d2
AndyPham1 <andypham2341@hotmail.com>
2015-11-25 01:34:42 -0500

    Create listview.cpp

---
+++ src/main/c++/listview.cpp
@@ -0,0 +1,52 @@
+#include <QtCore>
+#include <QtGui>
+#include <QtCore>
+#include <QtGui>
+#include <QMessageBox>
+#include <QTreeWidget>
+#include "listview.h"
+#include "pubTree.h"
+#include <QPainter>
+#include <vector>
+#include <QColor>
+#include <iostream>
+
+
+
+using namespace std;
+
+
+ListView::ListView(QWidget *parent) : QTreeWidget(parent)
+{
+
+	
+}
+
+void ListView::makeList(node * root)
+{
+	this->setColumnCount(2);
+	this->setHeaderLabels(QStringList() << "Col A" << "Col B");
+//	QTreeWidgetItem *itm = new QTreeWidgetItem(this);
+//	itm->setText(0, "Professor");
+//	itm->setText(1, "Publications");
+//	addChild(itm, "Author 1", "57");
+
+	vector<node*>* children = root->getChildren();
+	int size = children->size();
+	for (int i = 0; i < size; i++)
+	{
+		QTreeWidgetItem *itm = new QTreeWidgetItem(this);
+		itm->setText(0, QString::fromStdString(children->at(i)->getFirst()));
+		itm->setText(1,QString::number(children->at(i)->getSecond()));
+	}
+		nodeData = root;
+	nodeData->getFirst();
+}
+
+void ListView::addChild(QTreeWidgetItem* parent, QString name, QString description)
+{
+	QTreeWidgetItem *itm = new QTreeWidgetItem();
+	itm->setText(0, name);
+	itm->setText(1, description);
+	parent->addChild(itm);
+}
